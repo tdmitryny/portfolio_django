4b2d9756d2de0fe717dd1a33d67e49bebbfb5fb5
Dmitry <tdmitryny@gmail.com>
2024-07-13 13:42:26 -0400

    Footer made

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
+MEDIA_URL = '/images/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
