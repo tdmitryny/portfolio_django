72d2898ae1535feff4c4ca1b4af7fb7495a12ef3
Dmitry <tdmitryny@gmail.com>
2024-07-17 10:54:15 -0400

    Creating database

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, blank=True)
+    date = models.DateTimeField(default=timezone.now)
+    content = models.TextField(max_length=2000, blank=True)
+    
+    class Meta:
+        ordering = ['-date']
+    
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,26 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
+
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Testings',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+     {
+        'author': 'Alex',
+        'title': 'New Testings',
+        'content': 'Second post content',
+        'date_posted': 'September 27, 2018'
+    }
+]
+
 def home(request):
     return render(request, 'home.html')
 
 def post(request):
-    return render(request, 'blog/post.html')
+    contex = {'posts': posts}
+    return render(request, 'blog/post.html', contex)
