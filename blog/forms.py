093999ef546657d9d4ca318991bf2995778ac4a0
Dmitry <tdmitryny@gmail.com>
2024-08-10 14:42:39 -0400

    css contact form

--- blog/forms.py
+++ blog/forms.py
@@ -4,9 +4,9 @@ from django import forms
 
 class ContactForm(forms.Form):
     email = forms.EmailField(
-        widget=forms.TextInput(attrs={"placeholder": "Your e-mail"})
+        widget=forms.TextInput(attrs={"placeholder": "Your e-mail", "class": "form-control"})
     )
-    subject = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "Subject"}))
+    subject = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "Subject", "class": "form-control"}))
     message = forms.CharField(
-        widget=forms.Textarea(attrs={"placeholder": "Your message"})
+    widget=forms.Textarea(attrs={"placeholder": "Your message", "class": "form-control"})
     )
